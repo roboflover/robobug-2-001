db608864bc6e6353e6f8e81ca6ec5595c4c80da1
roboflover <96958044+roboflover@users.noreply.github.com>
2023-10-07 02:35:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import { WebSocketServer } from 'ws';
+import si from "systeminformation";
+
+const wss = new WebSocketServer({ port: 8080 });
+const mysql = require("mysql2")
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "usersdb",
+    password: "1q2w3e4r"
+})
+
+console.log('We are live on 8080');
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+
+  });
+
+  ws.send('something');
+
+  setInterval(async () => {
+    const cpuTemp = JSON.stringify(await si.currentLoad());
+    ws.send(cpuTemp);
+  }, 1000);
+});
+
